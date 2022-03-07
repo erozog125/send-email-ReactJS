077cd94a177cf222f2e77ea8aa3f1fa19ebe2670
Edwin Rozo Gómez <erozog125@gmail.com>
2022-03-06 23:05:50 -0500

    modal en ok

--- src/components/MailerJs/MailerJS.jsx
+++ src/components/MailerJs/MailerJS.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import emailjs from 'emailjs-com';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 export const MailerJS = () => {
 
+  const [style, setStyle] = useState("modal-submit-ok");
+
   const sendEmail = (e) => {
+
     e.preventDefault();
     console.log(e.target);
     emailjs.sendForm('service_yx1ennh','template_7hxi21k',e.target,'Zjo6mNYvNam9PYTZC')
-    .then(response => console.log(response))
+    .then(response => {
+          setStyle("visible");
+          console.log(response);
+          setTimeout(() => {
+            setStyle("modal-submit-ok")
+          }, 1500);                          
+    })
     .catch( error => console.log(error))
   }
 
   return (
     <div className='div-form'>
-      <h1 className='title-form'>Send Email</h1>
+      <h1 className='title-form'>Contact Us</h1>
       <form className='form-mail' onSubmit={sendEmail}>
         <label>Name</label>
         <input type="text" name='user_name' />
@@ -28,6 +37,11 @@ export const MailerJS = () => {
         <hr />
         <button>Send</button>
       </form>
+      <div id='modal' className={style}>
+        <div className="icon">
+          <h2 id='h2-msj'>Sent!</h2>      
+        </div>
+      </div>
     </div>
   )
 }
